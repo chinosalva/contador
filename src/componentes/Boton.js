f19023d4db18618ebede2a0f5d9fbb7ec4e83cde
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-07-25 01:25:39 -0500

    Contador

---
+++ src/componentes/Boton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Boton ({ texto, esBotonDeClic }) {
+    return (
+        <button
+        className=''
+        >
+            {texto}
+        </button>
+    )
+}
+export default Boton
